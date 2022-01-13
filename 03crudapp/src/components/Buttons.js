6372c7a4c57a6652c800ca3b86e59066ff2bbdbd
TaeimShin <poroon7@gmail.com>
2022-01-13 18:24:17 +0900

    mm

--- 03crudapp/src/components/Buttons.js
+++ 03crudapp/src/components/Buttons.js
@@ -1,32 +1,33 @@
-import React, {Component, useLayoutEffect} from "react";
+import React, {Component} from "react";
 
-class Buttons extends Component{
+class Buttons extends Component {
     render(){
         return (
-            /* 
-                react에서 style을 지정할때는 중괄호를 중첩해서 사용한다.
-
-            */
-            <ul>
-                <li style={{listStyleType:'none'}}>
-                    <input type="button" value="create" onClick={function(e){
-                        e.preventDefault();
-                        this.props.onChangeMode('create');
-                    }.bind(this)} 
-                    />
-                    <input type="button" value="update" onClick={function(e){
-                        e.preventDefault();
-                        this.props.onChangeMode('update');
-                    }.bind(this)}
-                    />
-                    <input type="button" value="delete" onClick={function(e){
-                        e.preventDefault();
-                        this.props.onChangeMode('delete');
-                    }.bind(this)}
-                    />
-                </li>
-            </ul>
+        /*
+        React에서 style을 지정할때는 중괄호를 중첩해서 사용한다. 
+            
+        */
+        <ul>
+            <li style={{listStyleType:'none'}}>
+                <input type="button" value="create" onClick={(e)=>{
+                    e.preventDefault();
+                    this.props.onChangeMode('create');
+                    }}
+                />
+                <input type="button" value="update" onClick={(e)=>{
+                    e.preventDefault();
+                    this.props.onChangeMode('update');
+                    }}
+                />
+                <input type="button" value="delete" onClick={(e)=>{
+                    e.preventDefault();
+                    this.props.onChangeMode('delete');
+                    }}
+                />
+            </li>            
+        </ul>
         );
     }
 }
-export default Buttons
+
+export default Buttons  
--- 03crudapp/src/components/CreateForm.js
+++ 03crudapp/src/components/CreateForm.js
@@ -1,26 +1,30 @@
 import React, {Component} from "react";
 
+/*
+새로운 게시물 작성을 위한 작성폼 컴포넌트 생성
+*/
 class CreateForm extends Component {
     render(){
-        return(
-            <article>
-                <h2>Create</h2>
-                <form action="/create_process" method="post" onSubmit={function(e){
-                    //submit되었을떄 화면의 새로고침 차단
-                    e.preventDefault();
-                    //부모가 내려준 props를 통해 폼값을 전달한다.
+        return (
+        <article>
+            <h2>Create</h2>
+            <form action="/create_process" method="post" onSubmit={(e)=>{
+                    //submit되었을때 화면의 새로고침 차단
+                    e.preventDefault(); 
+                    //부모가 내려준 props를 통해 폼값을 전달한다. 
                     this.props.onSubmitValue(
                         e.target.title.value,
                         e.target.desc.value
-                    )
-                }.bind(this)}>
-                    <p><input type="text" name="title" placeholder="제목입력"></input></p>
-                    <p><textarea name="desc" placeholder="내용입력"></textarea></p>
-                    <p><input type="submit" value="전송"></input></p>
-                </form>
-            </article>
-        )
+                    );
+                }
+            }>
+                <p><input type="text" name="title" placeholder="제목입력" /></p>
+                <p><textarea name="desc" placeholder="내용입력"></textarea></p>
+                <p><input type="submit" value="전송" /></p>
+            </form>
+        </article>
+        );
     }
 }
 
-export default CreateForm
+export default CreateForm  
